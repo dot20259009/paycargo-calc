3142e75e6162c983b721144392fef97482bb5f3c
dot20259009 <onlywork1020@gmail.com>
2025-05-05 14:26:33 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import AFKLMPPayCargoCalculator from "./components/AFKLMPPayCargoCalculator";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <AFKLMPPayCargoCalculator />
+    </div>
+  );
+}
+
+export default App;
